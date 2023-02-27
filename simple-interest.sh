7c91af07dba443d3e367a22c2c0814e12673e095
Juan Villanueva <90359433+JuanVillanuevaTabares@users.noreply.github.com>
2023-02-26 19:40:08 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,7 +6,7 @@
 
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Juan Villanueva / juanvillanueva.tabares@gmail.com
 
    # Input:
    # p, principal amount
